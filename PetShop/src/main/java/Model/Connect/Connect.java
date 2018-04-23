3e79b8645b2ab698f1208e889c4f7a06e29dde70
Joaorocha92 <joaorocha92@hotmail.com>
2018-04-23 20:32:13 -0300

    Add files via upload

---
+++ PetShop/src/main/java/Model/Connect/Connect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author dev
+ */
+public class Connect {
+
+    public static Connection connect() {
+
+        try {
+            String driver = "com.mysql.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/petshop";
+            String user = "root";
+            String password = "";
+
+            Class.forName(driver);
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException | ClassNotFoundException sc) {
+            return null;
+        }
+    }
+}
---
+++ PetShop/src/main/java/com/javadbc/petshoppi/model/ModelCommercialService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javadbc.petshoppi.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author victor.rslucca
+ */
+public class ModelCommercialService extends ModelCommercial{
+    
+    private String pet;
+
+    public ModelCommercialService(String pet, double valor, String nome, String filial) {
+        super(valor, nome, filial);
+        this.pet = pet;
+    }
+
+    public String getPet() {
+        return pet;
+    }
+
+    public void setPet(String pet) {
+        this.pet = pet;
+    }
+}
